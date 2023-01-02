1dd613abd79c8a3f2ac3eff730e38ecfd5cc8c40
MatysGuzy <matyseusz@gmail.com>
2023-01-02 19:33:18 +0100

    Create index

---
+++ index.php
@@ -0,0 +1,50 @@
+<?php
+    session_start();
+?>
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta charset="utf-8">
+        <link rel="stylesheet" href="style.css" type="text/css">
+        <title>Blog</title>
+    </head>
+    <body>
+        <div class="content">
+            <header>
+                
+            </header>
+            <nav>
+                <ol>
+                    <li><a href="?opcja=stronaglowna">Strona Główna</a></li>
+                    <li><a href="?opcja=onas">O nas</a></li>
+                    <li><a href="?opcja=kontakt">Kontakt</a>
+                    <li><a href="?opcja=zaloguj">Zaloguj</a>
+                </ol>
+            </nav>
+            <main>
+                <?php
+                    if(isset($_GET['opcja'])) {
+                        if($_GET['opcja']=="stronaglowna") {
+                            include("php/stronaglowna.php");
+                        }
+                        if($_GET['opcja']=="onas") {
+                            include("php/onas.php");
+                        }
+                        if($_GET['opcja']=="kontakt") {
+                            include("php/kontakt.php");
+                        }
+                        if($_GET['opcja']=="zaloguj") {
+                            include("php/zaloguj.php");
+                        }
+
+                    } else {
+                        include("php/stronaglowna.php");
+                    }
+                ?>
+            </main>
+        </div>
+        <footer>
+            
+        </footer>
+    </body>
+</html>
