d88226621485615e79c0b247bcdd460dbcbf7d93
MatysGuzy <matyseusz@gmail.com>
2023-01-02 20:29:24 +0100

    Create dodajuzytkownika

---
+++ admin/php/dodajuzytkownika.php
@@ -0,0 +1,17 @@
+<table>
+    <tr>
+        <td>Imie</td>
+        <td>Nazwisko</td>
+        <td>Login</td>
+        <td>Hasło</td>
+    </tr>
+    <form method="POST" action="?opcja=stronaglowna" id="formularz">
+        <tr>
+            <td><input type="text" name="imie"/></td>
+            <td><input type="text" name="nazwisko"/></td>
+            <td><input type="text" name="login"/></td>
+            <td><input type="password" name="haslo"/></td>
+        </tr>
+</table>
+<input type="submit" name="zaloz" value="Załóż" />
+</form>
