27819b1354974796c370e8249917d81d64078a8e
Kral1312 <mateusz.kral03@gmail.com>
2023-01-02 20:25:57 +0100

    create dodajartykul

---
+++ admin/php/dodajartykul.php
@@ -0,0 +1,8 @@
+<form action="?opcja=stronaglowna" method="post">
+    Podaj tytuł artykuł: <br>
+    <input type="text" name="tytul" size="80" />
+    <br>Podaj treść artykułu: <br>
+    <textarea name="tresc" cols="60" rows="10"></textarea>
+    <br>
+    <input type="submit" name="dodaj" value="Dodaj" />
+</form>
