1cb4593b45982d57699286b901d4f28248a427b6
Kral1312 <mateusz.kral03@gmail.com>
2023-01-02 20:52:54 +0100

    create wyloguj

---
+++ admin/php/wyloguj.php
@@ -0,0 +1,26 @@
+<html>
+    <head>
+    <title>wyloguj</title>
+    </head>
+    <body>
+        <?php
+            $pol = mysqli_connect("localhost","root","","text");
+            if(!$pol) {
+                echo "Brak połączenia z BD";
+                exit();
+            } else {
+               $sql = "SELECT * 
+               FROM logowanie
+               WHERE login='$_SESSION[login]'";
+               $zap = mysqli_query($pol,$sql);
+               $dane = mysqli_fetch_array($zap);
+               echo "Użytkownik ". $dane['login']. " został wylogowany";
+               echo "<br>";
+               echo "Przejdź do strony głównej<br>";
+               echo "<a href="../index.php">Strona Główna</a>";
+               session_destroy();
+            }
+        ?>
+    </body>
+</html>
+﻿
